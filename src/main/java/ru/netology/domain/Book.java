e7551a4f0bbb3932c646f4c3ad586b152227a4b2
IlyaKosov2211 <ilakosov8@Gmail.com>
2022-02-02 19:37:40 +0700

    first commit

---
+++ src/main/java/ru/netology/domain/Book.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+public class Book extends Product {
+    private String author;
+    private String name;
+
+    public Book(int id, String name, int price, String author) {
+        super(id, name, price);
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+}
+
